48f289d35f86da483c25cccd90ff0461f9c205f8
Laura Gwen <lauraburch.lb@gmail.com>
2020-11-10 23:22:41 -0500

    added clear highscore button

--- assets/winner.js
+++ assets/winner.js
@@ -1,5 +1,6 @@
 var quizArr = JSON.parse(localStorage.getItem("quizHigh")) || []
 var fieldQuiz = document.getElementById("initalHighscore");
+var clearButton = document.getElementById("clear")
 console.log(quizArr)
 function highscore (){
     for (var i = 0; i < quizArr.length; i++) {
@@ -11,3 +12,11 @@ function highscore (){
 }
 
 highscore()
+
+function buttonClear(){
+    localStorage.clear() 
+    localStorage.removeItem("initalHighscore")
+    window.location.assign('./winner.html');
+}
+
+clearButton.addEventListener("click",buttonClear);
